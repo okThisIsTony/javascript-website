a4428d52a62481b3855c0c4eefe3fbd429af8527
Anthony Carzon <apjcarzon@gmail.com>
2023-10-31 13:43:01 -0400

    10th +otherButton funct, .css margin, variable

--- javascript.js
+++ javascript.js
@@ -1,4 +1,12 @@
 function onClick(inputName) {
   document.getElementById("output").innerHTML = "Welcome";
   alert("Welcome, " + inputName + "!");
+
+  const myVariableName = { name: "Adam" };
+  console.log(myVariableName.name);
+}
+
+function otherButton(inputName2) {
+  document.getElementById("output2").innerHTML = "Accepted";
+  alert("Email accepted: " + inputName2);
 }
