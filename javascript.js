3ec5cf3a0b2d55df371dc0a512eeed96abcc8607
Anthony Carzon <apjcarzon@gmail.com>
2023-10-31 13:55:44 -0400

    12th added list variable

--- javascript.js
+++ javascript.js
@@ -4,6 +4,9 @@ function onClick(inputName) {
 
   const myVariableName = { name: "Adam" };
   console.log(myVariableName.name);
+
+  const myList = [];
+  console.log(myList[7]);
 }
 
 function otherButton(inputName2) {
