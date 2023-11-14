712208118a7d9cb0ce673bd57c8a19054c407f1a
Anthony Carzon <apjcarzon@gmail.com>
2023-11-14 13:28:20 -0500

    13th added/changed myList & list in .js

--- javascript.js
+++ javascript.js
@@ -2,14 +2,14 @@ function onClick(inputName) {
   document.getElementById("output").innerHTML = "Welcome";
   alert("Welcome, " + inputName + "!");
 
-  const myVariableName = { name: "Adam" };
+  const myVariableName = { name: "Username:" + inputName };
   console.log(myVariableName.name);
-
-  const myList = [];
-  console.log(myList[7]);
 }
 
 function otherButton(inputName2) {
   document.getElementById("output2").innerHTML = "Accepted";
   alert("Email accepted: " + inputName2);
+
+  const myList = { list: "Email:" + inputName2 };
+  console.log(myList.list);
 }
