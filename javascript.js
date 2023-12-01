0734476d8de78f7398522e0eaa9b9680104cd237
Anthony Carzon <apjcarzon@gmail.com>
2023-12-01 11:47:14 -0500

    18th Made pokemon.name into <h2>

--- javascript.js
+++ javascript.js
@@ -1,18 +1,15 @@
 function onClick(inputName) {
-
   const url = "https://pokeapi.co/api/v2/pokemon?offset=0&limit=20";
   $.get(url, function (data, status) {
     if (status === "success") {
-      const pokemonNames = data.results.map(showName);
+      const pokemonNames = data.results.map((pokemon) => {
+        return "<h2>" + pokemon.name + "</h2>";
+      });
       console.log(pokemonNames);
-      document.getElementById("output").innerHTML = pokemonNames.join(" ");
+      document.getElementById("output").innerHTML = pokemonNames;
     }
   });
   alert("Welcome, " + inputName + "!");
-}
-
-function showName(pokemon) {
-  return pokemon.name;
 }
 
 function otherButton(inputName2) {
