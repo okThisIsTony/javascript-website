4631b56a5a2d7000380606f4befa29eacb57e434
Anthony Carzon <apjcarzon@gmail.com>
2023-12-12 13:51:16 -0500

    22nd Added else and h2Tag var

--- javascript.js
+++ javascript.js
@@ -3,10 +3,13 @@ function onClick(inputName) {
   $.get(url, function (data, status) {
     if (status === "success") {
       const pokemonNames = data.results.map((pokemon) => {
+        const h2Tag = document.createElement("h2");
         return "<h2>" + pokemon.name + "</h2>";
       });
       console.log(pokemonNames);
       document.getElementById("output").innerHTML = pokemonNames;
+    } else {
+      console.log("The status is a " + status + "!");
     }
   });
 }
