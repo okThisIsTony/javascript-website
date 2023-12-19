00102d8fbbc2f77cfe6fdddc08e0e7f3cc2f8f99
Anthony Carzon <apjcarzon@gmail.com>
2023-12-19 13:10:34 -0500

    23rd output to displayedText cleaned up .js

--- javascript.js
+++ javascript.js
@@ -2,12 +2,12 @@ function onClick(inputName) {
   const url = "https://pokeapi.co/api/v2/pokemon?offset=0&limit=20";
   $.get(url, function (data, status) {
     if (status === "success") {
-      const pokemonNames = data.results.map((pokemon) => {
-        const h2Tag = document.createElement("h2");
-        return "<h2>" + pokemon.name + "</h2>";
+      const pokemonNames = data.results.forEach((pokemon) => {
+        const h2Element = document.createElement("h2");
+        h2Element.innerHTML = pokemon.name;
+        console.log("None with h2 tag: ", h2Element);
+        document.getElementById("displayedText").append(h2Element);
       });
-      console.log(pokemonNames);
-      document.getElementById("output").innerHTML = pokemonNames;
     } else {
       console.log("The status is a " + status + "!");
     }
