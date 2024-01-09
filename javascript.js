038340b7dd2b1901d64dcc25c7f0f33593a57acf
Anthony Carzon <apjcarzon@gmail.com>
2024-01-09 13:47:24 -0500

    24th made addNamesToHTML

--- javascript.js
+++ javascript.js
@@ -2,14 +2,18 @@ function onClick(inputName) {
   const url = "https://pokeapi.co/api/v2/pokemon?offset=0&limit=20";
   $.get(url, function (data, status) {
     if (status === "success") {
-      const pokemonNames = data.results.forEach((pokemon) => {
-        const h2Element = document.createElement("h2");
-        h2Element.innerHTML = pokemon.name;
-        console.log("None with h2 tag: ", h2Element);
-        document.getElementById("displayedText").append(h2Element);
-      });
+      addNamesToHTML(data);
     } else {
       console.log("The status is a " + status + "!");
     }
   });
+}
+function addNamesToHTML(data) {
+  const pokemonNames = data.results.map((pokemon) => {
+    const h2Element = document.createElement("h2");
+    h2Element.innerHTML = pokemon.name;
+    console.log("None with h2 tag: ", h2Element);
+    return h2Element;
+  });
+  $("#displayedText").html(pokemonNames);
 }
